4f9e670f38424100cb1473d65255c5ef8f684322
Antonio GP - Matemáticas <agarcia.paredes2025@gmail.com>
2025-09-13 09:34:37 +0200

    Update 30_Genially.py

--- pages/30_Genially.py
+++ pages/30_Genially.py
@@ -41,4 +41,4 @@ html(f"""
     allowfullscreen>
   </iframe>
 </div>
-""", height=400)  # ← reserva en Streamlit (ajústalo si te queda corto)
+""", height=420)  # ← reserva en Streamlit (ajústalo si te queda corto)
