55579038ecc5c7d06fe60ec9526853bdc0832c2a
Antonio GP - Matemáticas <agarcia.paredes2025@gmail.com>
2025-09-13 08:25:36 +0200

    Update 30_Genially.py

--- pages/30_Genially.py
+++ pages/30_Genially.py
@@ -1,4 +1,20 @@
 
+# pages/30_Genially.py
+# -*- coding: utf-8 -*-
+
 import streamlit as st
-st.header("Portada")
-st.write("Bienvenido/a al Torneo Suizo Escolar (baseline seguro).")
+
+from lib.ui import sidebar_title_and_nav
+
+# NAV personalizada debajo de la cabecera (título + nivel/año)
+#sidebar_title_and_nav(extras=True)  # autodetecta páginas automáticamente
+sidebar_title_and_nav(
+    extras=True,
+    items=[
+        ("app.py", "♟️ Inicio"),
+        ("pages/10_Rondas.py", "🧩 Rondas"),
+        ("pages/20_Clasificacion.py", "🏆 Clasificación"),
+        ("pages/99_Administracion.py", "🛠️ Administración"),
+        ("pages/30_Genially.py", "♞ Genially")
+    ]
+)
