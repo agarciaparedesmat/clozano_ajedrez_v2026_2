7e7ffb2d196814fa224f1e7024c3f4eca5aadf35
Antonio GP - Matemáticas <agarcia.paredes2025@gmail.com>
2025-09-13 09:49:40 +0200

    Update 30_Genially.py

--- pages/30_Genially.py
+++ pages/30_Genially.py
@@ -29,7 +29,7 @@ st.markdown("""
 <style>
 /* Reduce el padding superior/inferior del contenedor principal */
 div.block-container { 
-  padding-top: 2.00rem !important; 
+  padding-top: 3.00rem !important; 
   padding-bottom: .30rem !important; 
 }
 /* Un pelín menos de separación entre bloques del main */
